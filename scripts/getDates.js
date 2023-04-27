a60c703de6f84b8ce67dcb1cb4411465ba2fc086
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-04-27 14:38:27 +0000

    fixed last_modified code

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,6 +3,5 @@ let current_year = document.querySelector(".current-year");
 current_year.textContent = new Date().getFullYear();
 
 // Last modified
-let last_modified = document.querySelector("#lastModified");
-last_modified.textContent = `Last Modification: ${Date(document.last_modified)}`
-// TODO: Fix this crap
+let last_modified = document.lastModified;
+document.querySelector("#lastModified").textContent = `Last Modification: ${last_modified}`;
