d67458b06aaa8876cbbd95d8f220543603e5a2bd
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-05-09 21:01:21 +0000

    added border color change to dark mode

--- scripts/dark.js
+++ scripts/dark.js
@@ -2,6 +2,7 @@
 const darkBtn = document.querySelector("#dark");
 const main = document.querySelector("main")
 const h3 = document.querySelectorAll("h3")
+const card = document.querySelectorAll(".card")
 
 // Make all h3 dark
 h3.forEach(function(e) {
@@ -9,14 +10,23 @@ h3.forEach(function(e) {
 })
 
 
+
 darkBtn.addEventListener("click", function() {
 
-	if (darkBtn.textContent.includes("✔️")) {		
+	if (darkBtn.textContent.includes("✔️")) {
+		// Make all card borders white
+		card.forEach(function(e) {
+			e.style.border = "1px solid white"
+		})
 		main.style.backgroundColor = "black"
 		main.style.color = "white"
 		darkBtn.textContent = "❎"
 		darkBtn.classList.toggle('darkMode');
 	} else {
+		// Make all card borders black
+		card.forEach(function(e) {
+			e.style.border = "1px solid black"
+		})
 		main.style.color = "black"
 		main.style.backgroundColor = "white"
 		darkBtn.textContent = "✔️"
