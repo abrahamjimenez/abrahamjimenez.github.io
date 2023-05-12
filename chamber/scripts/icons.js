aa15c278645a27a22039880b8f42c509e22fe7e3
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-05-12 20:40:53 +0000

    updated font awesome icons

--- chamber/scripts/icons.js
+++ chamber/scripts/icons.js
@@ -2,9 +2,9 @@ const icons = document.querySelectorAll("i");
 
 icons.forEach(function(i) {
 	i.addEventListener("mouseenter", function() {
-		i.classList.toggle("fa-bounce")
+		i.classList.toggle("fa-spin")
 	})
 	i.addEventListener("mouseleave", function() {
-		i.classList.toggle("fa-bounce")
+		i.classList.toggle("fa-spin")
 	})
 })
