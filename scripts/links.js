cbe846782c3fad4ef93d8222a0921cb7b6af57ad
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-06-20 10:10:26 -0600

    saving progress

--- scripts/links.js
+++ scripts/links.js
@@ -1,3 +1,31 @@
 const baseURL = "https://abrahamjimenez.github.io/wdd230/";
 const linksURL = "https://abrahamjimenez.github.io/wdd230/data/links.json";
 
+async function getLinks() {
+	const response = await fetch(linksURL);
+	const data = await response.json();
+	// console.log(data);
+	
+	displayLinks(data);
+}
+
+function displayLinks(weeks) {
+  weeks.weeks.forEach(week => {
+	const weekElement = document.createElement('div');
+	weekElement.textContent = `${week.week}: `;
+
+	const linksContainer = document.createElement('div');
+	const links = week.links.map(link => {
+	  const linkElement = document.createElement('a');
+	  linkElement.href = link.url;
+	  linkElement.textContent = link.title;
+	  return linkElement.outerHTML;
+	});
+
+	linksContainer.innerHTML = links.join(' | ');
+	weekElement.appendChild(linksContainer);
+	document.body.appendChild(weekElement);
+  });
+}
+
+getLinks();
