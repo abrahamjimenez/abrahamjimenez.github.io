94c2ff3a130cbf93e0151e4c5d5635d667132bea
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-06-20 11:44:12 -0600

    Added - p element using JS

--- scripts/links.js
+++ scripts/links.js
@@ -1,18 +1,21 @@
 const baseURL = "https://abrahamjimenez.github.io/wdd230/";
 const linksURL = "https://abrahamjimenez.github.io/wdd230/data/links.json";
 
+const learningActivies = document.querySelector("#learning__activities")
+
 async function getLinks() {
 	const response = await fetch(linksURL);
 	const data = await response.json();
 	// console.log(data);
-	
+
 	displayLinks(data);
 }
 
 function displayLinks(weeks) {
   weeks.weeks.forEach(week => {
 	const weekElement = document.createElement('div');
-	weekElement.textContent = `${week.week}: `;
+	const weekParagraphElement = document.createElement("p");
+	weekParagraphElement.textContent = `${week.week}: `;
 
 	const linksContainer = document.createElement('div');
 	const links = week.links.map(link => {
@@ -23,8 +26,9 @@ function displayLinks(weeks) {
 	});
 
 	linksContainer.innerHTML = links.join(' | ');
+	weekElement.appendChild(weekParagraphElement);
 	weekElement.appendChild(linksContainer);
-	document.body.appendChild(weekElement);
+	learningActivies.appendChild(weekElement);
   });
 }
 
