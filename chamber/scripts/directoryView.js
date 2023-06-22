2a50356168df7cee863975cff46a07986ea05f34
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-06-21 21:28:32 -0600

    Added - Summary
    Updated
    - CSS class names

--- chamber/scripts/directoryView.js
+++ chamber/scripts/directoryView.js
@@ -1,5 +1,6 @@
+// This file changes the .directory--btn--container children to .card--view or .list--view
 const cardBtn = document.querySelector(".directory__card");
-const listBtn = document.querySelector(".directory--list");
+const listBtn = document.querySelector(".directory__list");
 
 const folderContainer = document.querySelector(".card--view");
 
