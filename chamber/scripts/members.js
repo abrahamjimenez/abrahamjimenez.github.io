48d17fdcbf79f47b4b7d65d2beb0a2b34e5cedc8
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-06-21 20:32:52 -0600

    Added - JSON displayData() function

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -7,15 +7,56 @@ async function apiFetch() {
 	displayData(data);
 }
 
+const directoryContainer = document.querySelector(".directory--container");
+
 function displayData(data) {
 	const companies = data.companies
-	
+
 	companies.forEach(company => {
+
+		// Constant Variable Names
+		const companyName = company.name;
+		const companyImage = company.image;
+		const companyAddress = company.address;
+		const companyPhone = company.phone;
+		const companyWebsite = company.website;
+
+		// Company Image Creation
 		const image = document.createElement("img");
-		image.src = `images/${company.image}`
-		image.alt = "hi"
+		image.src = `images/${companyImage}`;
+		image.alt = company.name
+				
+		// Company Address Creation
+		const addressParagraph = document.createElement("p");
+		addressParagraph.textContent = companyAddress;
 		
-		console.log(image)
+		// Company Phone Creation
+		const phoneParagraph = document.createElement("p");
+		phoneParagraph.textContent = companyPhone;
+		
+		// Company Website Creation		
+		const websiteLink = document.createElement("a");
+		websiteLink.href = companyWebsite;
+		websiteLink.textContent = companyName;
+
+		// Div Element Creation
+		const div = document.createElement("div");
+		div.classList = "company--container"
+		div.appendChild(image)
+
+		// Add Paragraphs company container
+		div.appendChild(addressParagraph); // Address
+		div.appendChild(phoneParagraph); // Phone
+		div.appendChild(websiteLink); // Website
+
+		// Add Div's to main html
+		directoryContainer.appendChild(div);
+
+		// Information
+		// console.log(image)
+		// console.log(company.name)
+		// console.log(company.address)
+		// console.log(company.website)
 	})
 }
 
