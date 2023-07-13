e7fda59a7fc8cbba25d4d1d9649c5272f574c2d0
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-07-13 09:04:29 -0600

    enabled weather.js

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,6 +1,7 @@
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=43.82&lon=-111.79&units=imperial&appid=ce8e98408d5f839cb2ea42a22f473e5a';
 
 const weather = document.querySelector(".weather");
+const weatherLocation = document.querySelector(".weather-location");
 const currentTemp = document.querySelector('.current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
@@ -24,6 +25,8 @@ function displayResults(data) {
 	weatherIcon.setAttribute('src', iconsrc);
 	weatherIcon.setAttribute('alt', desc);
 	captionDesc.textContent = `${desc}`;
+	weatherLocation.textContent = data.name;
 }
 
 apiFetch();
+
